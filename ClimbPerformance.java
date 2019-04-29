f2b23358bed4a498ec267c61d232d34c2879253e
G31M3R <matasgeimer@gmail.com>
2019-04-29 18:37:15 -0500

    Finished ClimbPerformance.java with minor testing; Added most of calculations to Leg.java

--- ClimbPerformance.java
+++ ClimbPerformance.java
@@ -1,7 +1,9 @@
 public class ClimbPerformance {
 
-	private static final double[][] timeFuelDistanceToClimb = {{74,730,0,0,0}, {73,695,1,.4,2}, {73,655,3,.8,4}, {73,620,4,1.2,6}, {73,600,6,1.5,8}, {73,550,8,1.9,10}, {73,505,10,2.2,13},
+	private static final double[][] timeFuelDistanceToClimb = {{74,730,0,0,0}, {73,695,1,0.4,2}, {73,655,3,0.8,4}, {73,620,4,1.2,6}, {73,600,6,1.5,8}, {73,550,8,1.9,10}, {73,505,10,2.2,13},
 	{73,455,12,2.6,16}, {72,410,14,3.0,19}, {72,360,17,3.4,22}, {72,315,20,3.9,27}, {72,265,24,4.4,32}, {72,220,28,5.0,38}};
+	private int rawStartingAlt;
+	private int rawEndingAlt;
 	private int startingAltitude;
 	private int endingAltitude;
 	private int climbSpeed;
@@ -12,20 +14,35 @@ public class ClimbPerformance {
 
 	public ClimbPerformance(int startingAltitude, int endingAltitude) {
 		//Takes the altitudes, divides them by 1000 and rounds, and uses that number as the index for performance calculations
+		this.rawStartingAlt = startingAltitude;
+		this.rawEndingAlt = endingAltitude;
 		this.startingAltitude = (int)(((double)(startingAltitude)/1000) + .5);
 		this.endingAltitude = (int)(((double)(endingAltitude)/1000) + .5);
-		this.climbSpeed = (int)((timeFuelDistanceToClimb[endingAltitude][0] + timeFuelDistanceToClimb[startingAltitude][0]) / 2);
-		this.climbRate = (timeFuelDistanceToClimb[endingAltitude][1] + timeFuelDistanceToClimb[startingAltitude][1]) / 2;
-		this.timeToClimb = timeFuelDistanceToClimb[endingAltitude][2] - timeFuelDistanceToClimb[startingAltitude][2];
-		this.fuelUsedToClimb = (int)((timeFuelDistanceToClimb[endingAltitude][3] - timeFuelDistanceToClimb[startingAltitude][3]) * 100 + .5) / 100;
-		this.distanceToClimb = timeFuelDistanceToClimb[endingAltitude][4] - timeFuelDistanceToClimb[startingAltitude][4];
+		this.climbSpeed = (int)((timeFuelDistanceToClimb[this.endingAltitude][0] + timeFuelDistanceToClimb[this.startingAltitude][0]) / 2);
+		this.climbRate = (timeFuelDistanceToClimb[this.endingAltitude][1] + timeFuelDistanceToClimb[this.startingAltitude][1]) / 2;
+		this.timeToClimb = timeFuelDistanceToClimb[this.endingAltitude][2] - timeFuelDistanceToClimb[this.startingAltitude][2];
+		this.fuelUsedToClimb = timeFuelDistanceToClimb[this.endingAltitude][3] - timeFuelDistanceToClimb[this.startingAltitude][3];
+		this.fuelUsedToClimb = (double)((int)(this.fuelUsedToClimb * 100 + .5)) / 100;
+		this.distanceToClimb = timeFuelDistanceToClimb[this.endingAltitude][4] - timeFuelDistanceToClimb[this.startingAltitude][4];
 	}
 
-	public int getStartingAltitude() {return startingAltitude;}
-	public int getEndingAltitude() {return endingAltitude;}
+	public int getStartingAltitude() {return rawStartingAlt;}
+	public int getEndingAltitude() {return rawEndingAlt;}
 	public int getClimbSpeed() {return climbSpeed;}
 	public double getClimbRate() {return climbRate;}
-	public double timeToClimb() {return timeToClimb;}
+	public double getTimeToClimb() {return timeToClimb;}
 	public double getFuelUsedToClimb() {return fuelUsedToClimb;}
 	public double getDistanceToClimb() {return distanceToClimb;}
+
+	public static void main(String[] args) {
+		System.out.println("Creating: ClimbPerformance(1000, 2000)");
+		ClimbPerformance perf = new ClimbPerformance(1000, 2000);
+		System.out.println("Starting Alt: " + perf.getStartingAltitude());
+		System.out.println("Ending Alt: " + perf.getEndingAltitude());
+		System.out.println("Climb Speed: " + perf.getClimbSpeed());
+		System.out.println("Climb Rate: " + perf.getClimbRate());
+		System.out.println("Time To Climb: " + perf.getTimeToClimb());
+		System.out.println("Fuel used to Climb: " + perf.getFuelUsedToClimb());
+		System.out.println("Distance Used to Climb: " + perf.getDistanceToClimb());
+	}
 }
--- Leg.java
+++ Leg.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Leg {
 
 	//Declare variables
@@ -9,27 +11,101 @@ public class Leg {
 	private int windVelocity;
 	private int magneticVariation;
 	private boolean isClimbingLeg;
-	private int altitude;
+	private int startAltitude;
+	private int endAltitude;
 	private ClimbPerformance climbPerformance;
 	private double groundSpeed;
+	private double windCorrectionAngle;
 	private double trueHeading;
 	private double magneticHeading;
+	private double deviation;
 	private double courseHeading;
 	private double distance;
 	private double estimatedTimeEnroute;
 	private double estimatedFuelConsumption;
 
-	public Leg() {
-		this.trueCourse = 0;
-		this.trueAirSpeed = 0;
-		this.altitude = 0;
-		this.distance = 0;
-	}
-
-	public Leg(int trueCourse, int trueAirSpeed, int altitude, int distance) {
+	public Leg(String startPoint, String endPoint, int trueCourse, int trueAirSpeed, int startAltitude, int endAltitude, int distance, int windDirection, int windVelocity, int magneticVariation, double deviation) {
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
 		this.trueCourse = trueCourse;
 		this.trueAirSpeed = trueAirSpeed;
-		this.altitude = altitude;
+		this.startAltitude = startAltitude;
+		this.endAltitude = endAltitude;
 		this.distance = distance;
+		this.windDirection = windDirection;
+		this.windVelocity = windVelocity;
+		this.magneticVariation = magneticVariation;
+		this.deviation = deviation;
+		this.calculatePerformance();
 	}
+
+	private void calculatePerformance() {
+		if(startAltitude != endAltitude) {
+			isClimbingLeg = true;
+			climbPerformance = new ClimbPerformance(startAltitude, endAltitude);
+		} else {isClimbingLeg = false;}
+		windCorrectionCalculation();
+		magneticHeadingCalculation();
+		courseHeadingCalculation(deviation);
+	}
+
+	private void windCorrectionCalculation() {
+		int windVector;
+		//Find the vector of wind b/c wind is given in from format instead of to format (eg "winds out of xxx @ xx")
+		if(windDirection <= 180) {
+			windVector = windDirection + 180;
+		} else {
+			windVector = windDirection - 180;
+		}
+
+		//Find the angle between trueCourse and windVector
+		double windAngle;
+		if(trueCourse == windVector) {windAngle = 0.0;}
+		else if(trueCourse > windVector) {
+			windAngle = trueCourse - windVector;
+		} else {
+			windAngle = windVector - trueCourse;
+		}
+		windAngle = Math.toRadians(windAngle);
+		double windCorrectionForce = windVelocity * Math.sin(windAngle); //Find the y component of the wind vector
+		double windCorrectionSpeed = windVelocity * Math.cos(windAngle); //Find the x component of the wind vector
+		double groundSpeed = windCorrectionSpeed + trueAirSpeed; //Find groundspeed by adding wind and true airspeed components
+		groundSpeed = (double)((int)(groundSpeed * 100)) / 100; //Round groundspeed
+		double windCorrectionAngle = Math.atan(windCorrectionForce / groundSpeed); //Find wind correction angle as arctan(windCorrectionForce / groundSpeed)
+		windCorrectionAngle = Math.toDegrees(windCorrectionAngle); //Convert wind correction angle to degrees
+		windCorrectionAngle = (double)((int)(windCorrectionAngle * 100)) / 100; //Round wind correction angle
+		this.groundSpeed = groundSpeed; //Set leg's groundspeed
+		this.windCorrectionAngle = windCorrectionAngle; //Set leg's wind correction angle
+		trueHeading = trueCourse + windCorrectionAngle; //Set leg's true heading
+	}
+
+	private void magneticHeadingCalculation() {
+		magneticHeading = trueHeading + magneticVariation;
+	}
+
+	private void courseHeadingCalculation(double deviation) {
+		this.deviation = deviation;
+		this.courseHeading = magneticHeading + deviation;
+	}
+
+	public String getStartPoint() {return startPoint;}
+	public String getEndPoint() {return endPoint;}
+	public int getTrueCourse() {return trueCourse;}
+	public int getTrueAirspeed() {return trueAirSpeed;}
+	public int getWindDirection() {return windDirection;}
+	public int getWindVelocity() {return windVelocity;}
+	public int getMagneticVariation() {return magneticVariation;}
+	public boolean getIsClimbingLeg() {return isClimbingLeg;}
+	public int getStartAltitude() {return startAltitude;}
+	public int getEndAltitude() {return endAltitude;}
+	public ClimbPerformance getClimbPerformance() {return climbPerformance;}
+	public double getGroundSpeed() {return groundSpeed;}
+	public double getWindCorrectionAngle() {return windCorrectionAngle;}
+	public double getTrueHeading() {return trueHeading;}
+	public double getMagneticHeading() {return magneticHeading;}
+	public double getDeviation() {return deviation;}
+	public double getCourseHeading() {return courseHeading;}
+	public double getDistance() {return distance;}
+	public double getEstimatedTimeEnroute() {return estimatedTimeEnroute;}
+	public double getEstimatedFuelConsumption() {return estimatedFuelConsumption;}
 }
